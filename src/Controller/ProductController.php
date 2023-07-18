012994c5cd29d594c00ac78064160bbcc09b1e74
Bakyon <nhok.kyo.9x@gmail.com>
2023-07-18 22:54:49 +0700

    Update image upload function

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use App\Form\ProductType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,7 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
-    #[Route('/products', name: 'app_products')]
+    #[Route('/', name: 'app_products')]
     public function index(ManagerRegistry $doctrine): Response
     {
         //$products = $doctrine->getRepository('App\Entity\Product')->findAll();
@@ -27,6 +28,7 @@ class ProductController extends AbstractController
     #[Route('/product/detail/{id}', name: 'app_product_detail')]
     public function detailsAction(ManagerRegistry $doctrine, $id): Response
     {
+        
         //$product = $doctrine->getRepository('App\Entity\Product')->find($id);
         $product = $doctrine->getRepository('App\Entity\Product')->find($id);
 
@@ -59,7 +61,33 @@ class ProductController extends AbstractController
 
         $form->handleRequest($request);
 
+        $uploadImg = $form['image']->getData();
+
         if ($form->isSubmitted() && $form->isValid()) {
+            //Handle uploaded image if needed
+            if ($uploadImg) {
+                $destination = $this->getParameter('kernel.project_dir').'/public/Images';
+                $originalImgName = pathinfo($uploadImg->getClientOriginalName(), PATHINFO_FILENAME);
+                $newImgName = $slugger->slug($originalImgName).'-'.uniqid().'.'.$uploadImg->guessExtension();
+
+                try {
+                    $uploadImg->move(
+                        $destination,
+                        $newImgName
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash(
+                        'error',
+                        'Cannot upload'
+                    );
+                }
+                $product->setImage($newImgName);
+            } else {
+                $this->addFlash(
+                    'error',
+                    'Cannot upload'
+                );
+            }
             $em = $doctrine->getManager();
             $em->persist($product);
             $em->flush();
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,7 +22,10 @@ class ProductType extends AbstractType
             ->add('name', TextType::class)
             ->add('description', TextareaType::class)
             ->add('owner', TextType::class)
-            ->add('image', TextType::class)
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+            ])
             ->add('size', choiceType::class, [
                 'choices' => [
                     'S' => 0,
